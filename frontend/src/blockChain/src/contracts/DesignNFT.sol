be7b298a48951c59f4d6f705513537e564244b1d
abm000 <112244qQ@>
2022-05-09 00:29:35 +0200

    - infoDesigsnByAddress mapping is not needed anymore because that is already applied in ERC721Enumerable getHistory,getCurrent and safeTransferFrom functionalities are added

--- frontend/src/blockChain/src/contracts/DesignNFT.sol
+++ frontend/src/blockChain/src/contracts/DesignNFT.sol
@@ -9,14 +9,16 @@ contract DesignNFT is ERC721Enumerable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
   // clothing proporties
-  mapping(uint => Design) private infoDesignByToken;
-  mapping(address => Design[]) private infoDesigsnByAddress;
+  // journey of token id. the key id the token id and value is the array indicates changes happend to the id. the last one is the most current
+  mapping(uint => Design[]) private infoDesignByToken;
   /*
   when multiple owners exist, the owner and all partners need to agree on transfering the nft.
   the key is the nft id what the process is about.
   the value is a map. the key specifies the address which is a form of partnership and the value is a boolean which is the agreement.
   */
   mapping(uint256 => mapping(address => bool)) private transferNftAgreement;
+  // register if the owners are registered to the token id to verify the agreement
+  // not needed anymore! it can be replaced by viewing desing struct, all info of designers are stored there.
   mapping(uint256 => mapping(address => bool)) private ownersExist;
 
   constructor() ERC721Enumerable("Design", "AMFI") {}
@@ -28,28 +30,35 @@ contract DesignNFT is ERC721Enumerable {
     _tokenIds.increment();
 
     uint256 newItemId = _tokenIds.current();
-    _mintSafeMain(owner, newItemId,tokenURI);
+    _mintSafeMain(owner, newItemId, tokenURI);
     //adding partners
-    address[] memory lDesignersincOwner = new address[](lDesigners.length+1);
+    address[] memory lDesignersincOwner = new address[](lDesigners.length + 1);
+    // reserve fist index for the true buyer
     lDesignersincOwner[0] = owner;
-    for(uint i =1; i<lDesigners.length+1;i++){
-      lDesignersincOwner[i] = lDesigners[i-1];
+    for (uint i = 1; i < lDesignersincOwner.length; i++) {
+      /* we begin with index 1 becuase the first index is reserved for the true buyer
+      and we copy all data from lDesigners to lDesignersincOwner. with doing this we include all involved buyers in one array
+      */
+
+      // prevent duplicate owner in lDesignersincOwner[]
+      if (lDesigners[i - 1] != owner) {
+        lDesignersincOwner[i] = lDesigners[i - 1];
+      }
     }
-    Design memory design = Design(name,lDesigners,tokenURI,newItemId);
-    infoDesignByToken[newItemId] = design;
+    Design memory design = Design(name, lDesigners, tokenURI, newItemId);
+    infoDesignByToken[newItemId].push(design);
     // register owners to the token id
     for (uint i = 0; i < lDesigners.length; i++) {
       ownersExist[newItemId][lDesigners[i]] = true;
     }
-    infoDesigsnByAddress[owner].push(design);
     return newItemId;
   }
   //Get all Nfts of the user
-  function getOwnedNFTs(address owner) public view returns (string[] memory){
-    uint numberOwnedNfts= infoDesigsnByAddress[owner].length;
-    string[] memory nfts = new string[](numberOwnedNfts);
-    for (uint256 i = 0; i < numberOwnedNfts; i++) {
-      nfts[i] = infoDesigsnByAddress[owner][i].URI;
+  function getOwnedURIs(address owner) public view returns(string[] memory){
+    uint256[] memory nftsIds = _ownedTokensIds(owner);
+    string[] memory nfts = new string[]( nftsIds.length);
+    for(uint256 i = 0; i< nftsIds.length; i++){
+      nfts[i] = tokenURI(nftsIds[i]);
     }
     return nfts;
   }
@@ -58,5 +67,18 @@ contract DesignNFT is ERC721Enumerable {
 
     require(ownersExist[tokenId][partner] == true, string(abi.encodePacked("Error: address", partner, "is not a part of ownership for this token id:", tokenId)));
     transferNftAgreement[tokenId][partner] = true;
+  }
+  //get history
+  function getHistory(uint tokenId)public view returns( Design[] memory){
+    return infoDesignByToken[tokenId];
+  }
+  // get current
+  function getCurrent(uint tokenId)public view returns( Design memory){
+    uint indexOfcurrentUser = infoDesignByToken[tokenId].length -1;
+    return infoDesignByToken[tokenId][indexOfcurrentUser];
+  }
+  //transfer token
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public  override(IERC721) {
+    super.safeTransferFrom(from, to,tokenId,'0x00000000000000000000000000000000');
   }
 }
