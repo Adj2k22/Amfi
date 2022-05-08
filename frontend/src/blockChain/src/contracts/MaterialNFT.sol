5d5fe9eef09427f003031f55a8d9ebb34adb684a
abm000 <112244qQ@>
2022-05-09 00:31:24 +0200

    matrialNft is made, it's similar to DesignNft

--- frontend/src/blockChain/src/contracts/MaterialNFT.sol
+++ frontend/src/blockChain/src/contracts/MaterialNFT.sol
@@ -1,8 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/utils/Counters.sol";
 import './ERC721Enumerable.sol';
+import '../structs/material.sol';
 
-contract DesignNFT is ERC721Enumerable {
-constructor() ERC721Enumerable("AndrehNFT", "AFT") {}
+contract MaterialNFT is ERC721Enumerable {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  mapping(uint => Material[]) private infoMaterialByToken;
+  mapping(address => Material[]) private infoMaterialByAddress;
+
+  constructor() ERC721Enumerable("AndrehNFT", "AFT") {}
+
+
+  function makeNFT(address owner, string calldata materialType, uint256 amount, string calldata tokenURI)
+  public
+  returns (uint256)
+  {require(amount>0,"Error your material needs to have weight ");
+    _tokenIds.increment();
+
+    uint256 newItemId = _tokenIds.current();
+    _mintSafeMain(owner, newItemId, tokenURI);
+    Material memory material = Material(owner, materialType, amount, tokenURI, newItemId);
+    infoMaterialByToken[newItemId].push(material);
+    infoMaterialByAddress[owner].push(material);
+    return newItemId;
+  }
 }
