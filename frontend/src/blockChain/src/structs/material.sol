3c9b064fc60fc775aee0a28f2d182f61bac06d92
abm000 <112244qQ@>
2022-05-09 00:30:24 +0200

    address[] ownedBy,TokenIndex has been removed

--- frontend/src/blockChain/src/structs/material.sol
+++ frontend/src/blockChain/src/structs/material.sol
@@ -1,10 +1,9 @@
 pragma solidity ^0.8.0;
 
-contract Material {
-  string name;
-  uint amount;
-  address[] ownedBy;
+struct Material {
+  address owner;
+  string materialType;
+  uint256 amount;
   string URI;
   uint tokenId;
-  uint TokenIndex;
 }
