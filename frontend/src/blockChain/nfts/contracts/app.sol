cc215e50d1cde3857fba1828f3e6e56a6a18df9a
abm000 <112244qQ@>
2022-05-17 02:52:46 +0200

    more preparation for the final sol app.

--- frontend/src/blockChain/nfts/contracts/app.sol
+++ frontend/src/blockChain/nfts/contracts/app.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.8.0;
 
 contract app {
-  function mintMaterial() public{}
+  // register all transactions
+  //mappings from any nft to transaction
+  // 998: register the parent of every child-> mapping
+  // take care of feedback change on 721 from 998 on custom mappings
+  function mintMaterial() public{
+  }
   function mintDesign() public{}
-  function addMaterialToDesign() public{}
+  function addMaterialToDesign(address contract998, address contractMaterial721) public{
+  //ComposableTopDown(contract998);
+
+  }
   function getIdGeneratorAddress()public view{}
   // history of transactions
+  function getMaterialFromDesign()public{}
 }
