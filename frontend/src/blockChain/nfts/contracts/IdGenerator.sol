396f419648262370d4b939a0eaa6a862907c4e18
abm000 <112244qQ@>
2022-05-21 21:52:12 +0200

    isContract() to check if the calling address is a contract mappings to store every generated tokeId type.
    givemeid() has been modified to get the nfttype from the calling contract and store it in the mapping to track it
    
    getNftType to get nftType from the mapping

--- frontend/src/blockChain/nfts/contracts/IdGenerator.sol
+++ frontend/src/blockChain/nfts/contracts/IdGenerator.sol
@@ -4,16 +4,40 @@ pragma solidity ^0.8.0;
 
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-
-contract IdGenerator  {
+import './enums/nftTypes.sol';
+///////////////dont overwrite!!!!!!!!!!!!!!!!
+contract IdGenerator {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  function giveMeId() public returns(uint256){
+  mapping(uint256 => NftType) tokensNftType;
+
+  function giveMeId() public returns (uint256){
+    require(isContract(msg.sender), "ERROR: Only Contracts Can get Id");
     _tokenIds.increment();
+
+    (bool success, bytes memory returnData) =
+    address(msg.sender)
+    //makeNFT(address owner,  address IdGeneratorAddress, string calldata materialType, uint256 amount, string calldata tokenURI)
+    .call(abi
+    .encodeWithSignature("getNftType()"));
+    require(success, string.concat("ERROR: Call to contract:", string(abi.encodePacked(msg.sender)), " is Failed!"));
+    NftType nftTypeOfAddress = abi.decode(returnData, (NftType));
+    tokensNftType[_tokenIds.current()] = nftTypeOfAddress;
     return _tokenIds.current();
   }
-  function getCurrentMeId() public view returns(uint256){
+
+  function getCurrentMeId() public returns (uint256){
     return _tokenIds.current();
+  }
+
+  function getNftType(uint256 tokenId) public returns (NftType){
+    return tokensNftType[tokenId];
+  }
+
+  function isContract(address addr) public view returns (bool) {
+    uint size;
+    assembly {size := extcodesize(addr)}
+    return size > 0;
   }
 }
