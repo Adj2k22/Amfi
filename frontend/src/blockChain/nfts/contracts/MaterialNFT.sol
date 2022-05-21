c7ad23eccb73cf9989001ebf145235074186551a
abm000 <112244qQ@>
2022-05-21 21:54:35 +0200

    nftType constant and get nftType function it needs to be fixed the constand to nftType.material

--- frontend/src/blockChain/nfts/contracts/MaterialNFT.sol
+++ frontend/src/blockChain/nfts/contracts/MaterialNFT.sol
@@ -3,9 +3,12 @@ pragma solidity ^0.8.0;
 
 import './ERC721Enumerable.sol';
 import './structs/material.sol';
-
+import './enums/nftTypes.sol';
 contract MaterialNFT is ERC721Enumerable {
-
+  NftType private nftType = NftType.BRAND;
+  function getNftType()public view returns(NftType){
+    return nftType;
+  }
   mapping(uint => Material[]) private infoMaterialByToken;
   mapping(address => Material[]) private infoMaterialByAddress;
 
@@ -18,7 +21,7 @@ contract MaterialNFT is ERC721Enumerable {
   {require(amount>0,"Error your material needs to have weight ");
     // making call to id generator to get id for the token
     (bool success, bytes memory returnData) = address(IdGeneratorAddress).call(abi.encodeWithSignature("giveMeId()"));
-    require(success, "Error: getting ID token faild");
+    require(success, "Error: getting ID token failed");
     uint256 newItemId = uint256(bytes32(returnData));
 
     _mintSafeMain(owner, newItemId, tokenURI);
