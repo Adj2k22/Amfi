0775c78a666e24aeaba13904a25c0803a1e6eff8
abm000 <112244qQ@>
2022-05-22 23:10:04 +0200

    mint function is done! -security code added: check if the minter is the owner. - connecting everything to mappings and arrays, so we can track the tokens easier

--- frontend/src/blockChain/998/contracts/ClothingC.sol
+++ frontend/src/blockChain/998/contracts/ClothingC.sol
@@ -17,13 +17,23 @@ contract ClothingC is ComposableTopDown {
   uint256 public test;
 
   function mint(address _to, uint256 tokenId, address IdGenerator) public returns (uint256) {
-
-
-    //makeNFT(address owner,  address IdGeneratorAddress, string calldata materialType, uint256 amount, string calldata tokenURI)
+    address tokenIdContract = getContractTokenId(IdGenerator, tokenId);
+    address tokenOwner = getOwnerTokenId(tokenIdContract, tokenId);
+    require(tokenOwner == msg.sender);
+    uint256 mintiedToken = super.mint(_to, tokenId);
+    // get the most current id from the generator to valid the tokenId
     uint256 id = uint256(getCurrentIDFromGenerator(IdGenerator));
+    // the id token id is not vaild if it is higher then the genarated id, becuase it is not generated yet!
     require(tokenId <= id, "Error: invalid tokenId");
+    NftType tokenType = NftType(getNftTypeOfTokenId(IdGenerator, tokenId));
+
+    if (tokenType == NftType.DESIGN) {
+      clothingTokens.push(tokenId);
+    }
+    tokenIdTransactions[tokenId].push(Transaction(now, msg.sender, address(this), tokenType, TokenIdAction.Created));
+
     // get nft type and push to mapping!!
-    return super.mint(_to, tokenId);
+    return mintiedToken;
   }
 
   enum TokenIdAction {Created, Moved, Composed}
@@ -34,7 +44,6 @@ contract ClothingC is ComposableTopDown {
     address to;
     NftType nftType;
     TokenIdAction tokenIdAction;
-    uint256 TokenId;
   }
 
   function getCurrentIDFromGenerator(address idGenerator) public returns (int value) {
@@ -62,9 +71,62 @@ contract ClothingC is ComposableTopDown {
     }
   }
 
-  function getNftTypeOfTokenId(address idGenerator, int tokenId) public view returns (uint256 value){
+
+  function getNftTypeOfTokenId(address idGenerator, uint256 tokenId) public view returns (uint8 value){
     address addr = address(idGenerator);
     bytes4 sig = bytes4(keccak256("getNftType(uint256)"));
+
+    assembly {
+      let o := mload(0x40) // Empty storage pointer
+      mstore(o, sig)
+      mstore(add(o, 0x04), tokenId)       // Push function signature to memory (function signature is 4 bytes/0x04)
+    //mstore(add(ptr,0x40), someInt32Argument); // Append function argument after signature
+    // From here, the call data size (input) would be functiona signature size + sum(argument size)
+    //   4bytes + 0 in this case, or 4bytes + 32bytes in the above commented `mstore`
+
+      let success := call(//This is the critical change (Pop the top stack value)
+      5000, //5k gas
+      addr, //To addr
+      0, //No value
+      o, //Inputs are stored at location x
+      0x44, //Inputs are 68 bytes long
+      o, //Store output over input (saves space)
+      0x20) //Outputs are 32 bytes long
+
+      value := mload(o) //Assign output value to c
+      mstore(0x40, add(o, 0x44)) // Set storage pointer to empty space
+    }
+  }
+
+  function getContractTokenId(address idGenerator, uint256 tokenId) public view returns (address value){
+    address addr = address(idGenerator);
+    bytes4 sig = bytes4(keccak256("getTokenContract(uint256)"));
+
+    assembly {
+      let o := mload(0x40) // Empty storage pointer
+      mstore(o, sig)
+      mstore(add(o, 0x04), tokenId)       // Push function signature to memory (function signature is 4 bytes/0x04)
+    //mstore(add(ptr,0x40), someInt32Argument); // Append function argument after signature
+    // From here, the call data size (input) would be functiona signature size + sum(argument size)
+    //   4bytes + 0 in this case, or 4bytes + 32bytes in the above commented `mstore`
+
+      let success := call(//This is the critical change (Pop the top stack value)
+      5000, //5k gas
+      addr, //To addr
+      0, //No value
+      o, //Inputs are stored at location x
+      0x44, //Inputs are 68 bytes long
+      o, //Store output over input (saves space)
+      0x20) //Outputs are 32 bytes long
+
+      value := mload(o) //Assign output value to c
+      mstore(0x40, add(o, 0x44)) // Set storage pointer to empty space
+    }
+  }
+
+  function getOwnerTokenId(address tokenIdContract, uint256 tokenId) public view returns (address value){
+    address addr = address(tokenIdContract);
+    bytes4 sig = bytes4(keccak256("ownerOf(uint256)"));
 
     assembly {
       let o := mload(0x40) // Empty storage pointer
