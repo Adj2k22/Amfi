00fe9736e693565e5e1a91c9a976a1d5361483ed
abm000 <112244qQ@>
2022-05-22 23:06:56 +0200

    -getNftType() changed to fit in all contract calls. -getTokenContract() get contract of some token for security reasons. -mappings added:  tokensExist (still not used because of calling bugs on 998). tokenContract.

--- frontend/src/blockChain/nfts/contracts/IdGenerator.sol
+++ frontend/src/blockChain/nfts/contracts/IdGenerator.sol
@@ -10,29 +10,40 @@ contract IdGenerator {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  mapping(uint256 => NftType) tokensNftType;
+  mapping(uint256 => NftType) private tokensNftType;
+  mapping(uint256 => bool) private tokensExist;
+  mapping(uint256 => address) private tokenContract;
 
   function giveMeId() public returns (uint256){
     require(isContract(msg.sender), "ERROR: Only Contracts Can get Id");
+
     _tokenIds.increment();
 
     (bool success, bytes memory returnData) =
     address(msg.sender)
-    //makeNFT(address owner,  address IdGeneratorAddress, string calldata materialType, uint256 amount, string calldata tokenURI)
     .call(abi
     .encodeWithSignature("getNftType()"));
     require(success, string.concat("ERROR: Call to contract:", string(abi.encodePacked(msg.sender)), " is Failed!"));
+
     NftType nftTypeOfAddress = abi.decode(returnData, (NftType));
     tokensNftType[_tokenIds.current()] = nftTypeOfAddress;
+    tokensExist[_tokenIds.current()] = true;
+    tokenContract[_tokenIds.current()] = msg.sender;
+
     return _tokenIds.current();
   }
 
-  function getCurrentMeId() public returns (uint256){
+  function getCurrentMeId() public view returns (uint256){
     return _tokenIds.current();
   }
 
-  function getNftType(uint256 tokenId) public returns (NftType){
-    return tokensNftType[tokenId];
+  function getNftType(uint256 tokenId) public view returns (uint8){
+    uint8 typeResult = uint8(tokensNftType[tokenId]);
+    return typeResult;
+  }
+
+  function getTokenContract(uint256 tokenId) public view returns (address){
+    return tokenContract[tokenId];
   }
 
   function isContract(address addr) public view returns (bool) {
