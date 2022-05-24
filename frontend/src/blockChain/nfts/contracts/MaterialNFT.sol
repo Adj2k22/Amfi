e00be8ae6b12d01dc6b2aab3dc053066811f98df
abm000 <112244qQ@>
2022-05-24 03:40:54 +0200

    code formatted

--- frontend/src/blockChain/nfts/contracts/MaterialNFT.sol
+++ frontend/src/blockChain/nfts/contracts/MaterialNFT.sol
@@ -4,21 +4,20 @@ pragma solidity ^0.8.0;
 import './ERC721Enumerable.sol';
 import './structs/material.sol';
 import './enums/nftTypes.sol';
+
 contract MaterialNFT is ERC721Enumerable {
-  NftType private nftType = NftType.BRAND;
-  function getNftType()public view returns(NftType){
-    return nftType;
-  }
+
+  NftType private constant nftType = NftType.MATERIAL;
+
   mapping(uint => Material[]) private infoMaterialByToken;
   mapping(address => Material[]) private infoMaterialByAddress;
 
   constructor() ERC721Enumerable("AndrehNFT", "AFT") {}
 
-
-  function makeNFT(address owner,  address IdGeneratorAddress, string calldata materialType, uint256 amount, string calldata tokenURI)
+  function makeNFT(address owner, address IdGeneratorAddress, string calldata materialType, uint256 amount, string calldata tokenURI)
   public
   returns (uint256)
-  {require(amount>0,"Error your material needs to have weight ");
+  {require(amount > 0, "Error your material needs to have weight ");
     // making call to id generator to get id for the token
     (bool success, bytes memory returnData) = address(IdGeneratorAddress).call(abi.encodeWithSignature("giveMeId()"));
     require(success, "Error: getting ID token failed");
@@ -29,5 +28,9 @@ contract MaterialNFT is ERC721Enumerable {
     infoMaterialByToken[newItemId].push(material);
     infoMaterialByAddress[owner].push(material);
     return newItemId;
+  }
+
+  function getNftType() public pure returns (NftType){
+    return nftType;
   }
 }
