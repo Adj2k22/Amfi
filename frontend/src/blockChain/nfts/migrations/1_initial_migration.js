95ec6c126ff9bffaba9a4b543ccc4cb65ddafb65
abm000 <112244qQ@>
2022-05-29 14:46:53 +0200

    app is no more

--- frontend/src/blockChain/nfts/migrations/1_initial_migration.js
+++ frontend/src/blockChain/nfts/migrations/1_initial_migration.js
@@ -2,11 +2,11 @@
 const DesignNFT = artifacts.require("./DesignNFT.sol");
 const MaterialNFT = artifacts.require("./MaterialNFT.sol");
 const IdGenerator = artifacts.require("./IdGenerator.sol");
-const App= artifacts.require("./App.sol");
+//const App= artifacts.require("./App.sol");
 module.exports = function (deployer) {
   /*deployer.deploy(Composable, "ComposableBottomUp", "COMPTD", {gas: 8000000});*/
   deployer.deploy(DesignNFT);
   deployer.deploy(MaterialNFT);
   deployer.deploy(IdGenerator);
-  deployer.deploy(App);
+  //deployer.deploy(App);
 };
